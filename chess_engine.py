a746fdb2f5939ce931c0c0fcfaa31a65123bcc2b
jalcumb <101678531+jacobalcu@users.noreply.github.com>
2025-11-05 23:08:48 -0500

    Implement basic minmax algo and test on dummy situations

--- chess_engine.py
+++ chess_engine.py
@@ -9,6 +9,13 @@ PIECE_VALUES = {
     # exclude king for now
 }
 
+# Score used for checkmate/stalemate to ensure engine prioritizes these outcomes
+INF_SCORE = 1000000
+
+# Initial search depth
+# Equals half-steps ahead
+MAX_DEPTH = 3
+
 
 class ChessEngine:
     """
@@ -30,7 +37,7 @@ class ChessEngine:
         print(self.board)
         print("\n----------------------------")
 
-    def evaluate_material(self) -> float:
+    def evaluate_material(self, board: chess.Board) -> float:
         """
         Calc material score of current board posistion
         Pos score means white is ahead
@@ -50,6 +57,116 @@ class ChessEngine:
 
         return score
 
+    def minimax(self, board: chess.Board, depth: int, is_maximizing_player: bool):
+        """
+        Recursive Minimax search function
+        Assumes White tries to maximize the score
+        and Black tries to minimize the score
+        """
+        # Base Case: Game over or depth reached
+        if depth == 0:
+            return self.evaluate_material(board)
+
+        if board.is_game_over():
+            result = board.outcome()
+
+            if result is None:
+                # Just to be safe, should never happen
+                return 0
+
+            # Checkmate handler
+            if board.is_checkmate():
+                # Loss for Maximizer is -INF
+                # Loss for Minimizer is +INF
+
+                # Winner determined by prev move
+                # Reward faster wins and delay faster losses by add/sub the depth
+                if result.winner == chess.WHITE:
+                    return INF_SCORE + depth
+                elif result.winner == chess.BLACK:
+                    return -INF_SCORE - depth
+
+            # Stalemate
+            return 0
+
+        # Recursive Step
+        # Maximizing Player
+        if is_maximizing_player:
+            max_eval = -INF_SCORE
+
+            # Iter thru ALL legal moves
+            for move in board.legal_moves:
+                # Make move on the board
+                board.push(move)
+                # Recursive call minimax for opponent
+                evaluation = self.minimax(board, depth - 1, False)
+                # Undo move to restore board state for next move search
+                board.pop()
+
+                # Update max score found so fat
+                max_eval = max(max_eval, evaluation)
+
+            return max_eval
+
+        # Minimizing player
+        else:
+            min_eval = INF_SCORE
+
+            # Iterate thru ALL legal moves
+            for move in board.legal_moves:
+                board.push(move)
+                evaluation = self.minimax(board, depth - 1, True)
+                board.pop()
+
+                min_eval = min(min_eval, evaluation)
+
+            return min_eval
+
+    def find_best_move(self, depth: int) -> chess.Move:
+        """
+        Finds best legal move for current player using minimax algo
+        """
+        # Score eval from Maximizers perspective
+        if self.board.turn == chess.WHITE:
+            best_eval = -INF_SCORE
+        else:
+            best_eval = INF_SCORE
+
+        best_move = None
+
+        # Det who current turn is
+        is_maximizing_player = self.board.turn == chess.WHITE
+
+        # Loop thru all possible root moves
+        for move in self.board.legal_moves:
+            # Make move
+            self.board.push(move)
+
+            # Call minimax for opponent
+            current_eval = self.minimax(self.board, depth - 1, not is_maximizing_player)
+
+            # Undo move
+            self.board.pop()
+
+            # Check if move is better than cur best
+            if is_maximizing_player:
+                if current_eval > best_eval:
+                    best_eval = current_eval
+                    best_move = move
+            else:
+                if current_eval < best_eval:
+                    best_eval = current_eval
+                    best_move = move
+
+        print(f"\nSearch complete (Depth {depth}). Best score found: {best_eval}")
+
+        if best_move:
+            print(f"Engine selected move: {best_move.uci()} (UCI)")
+        else:
+            print("No legal moves found")
+
+        return best_move
+
 
 # Main exe block to test setup
 if __name__ == "__main__":
@@ -59,48 +176,71 @@ if __name__ == "__main__":
     # Display the starting board
     engine.display_board()
     # Initial Evaluation (should be 0 as they are even)
-    initial_score = engine.evaluate_material()
-    print(f"Material Evaluation (Starting Board): {initial_score}")
+    # initial_score = engine.evaluate_material()
+    # print(f"Material Evaluation (Starting Board): {initial_score}")
 
+    # print(f"Finding best move for White (Depth {MAX_DEPTH})")
+    # best_move_white = engine.find_best_move(MAX_DEPTH)
+
+    # if best_move_white:
+    #     engine.board.push(best_move_white)
+    #     print(f"Board after engine plays {best_move_white.uci()}")
+    #     engine.display_board()
+
+    # Set up simple tactical position to check material gain
+    free_pawn_fen = "r2qkb1r/pp3ppp/2n2n2/3Pp3/2B1P3/2N2N2/PPP2PPP/R1BQK2R w KQkq - 0 8"
+    engine.board.set_fen(free_pawn_fen)
+
+    print(f"Test Pos: Free Pawn on e5 (Engine should take)")
+    engine.display_board()
+
+    print(f"Engine calc best move for White (Depth 3)")
+    best_move_tactical = engine.find_best_move(3)
+
+    if best_move_tactical:
+        engine.board.push(best_move_tactical)
+
+        print(f"Board after engine plays {best_move_tactical}")
+        engine.display_board()
     # Sample move to show board changing
     # 'e2e4' (UCI format) white pawn from e2 to e4
-    try:
-        move_e2e4 = chess.Move.from_uci("e2e4")
-        if move_e2e4 in engine.board.legal_moves:
-            engine.board.push(move_e2e4)
-            print("\n--- After White plays e2e4 ---")
-            engine.display_board()
-            score_after_e2e4 = engine.evaluate_material()
-            # Should still be 0
-            print(f"Material Evaluation: {score_after_e2e4}")
+    # try:
+    #     move_e2e4 = chess.Move.from_uci("e2e4")
+    #     if move_e2e4 in engine.board.legal_moves:
+    #         engine.board.push(move_e2e4)
+    #         print("\n--- After White plays e2e4 ---")
+    #         engine.display_board()
+    #         score_after_e2e4 = engine.evaluate_material()
+    #         # Should still be 0
+    #         print(f"Material Evaluation: {score_after_e2e4}")
 
-        else:
-            print("e2e4 is not legal")
+    #     else:
+    #         print("e2e4 is not legal")
 
-        move_d7d5 = chess.Move.from_uci("d7d5")
-        if move_d7d5 in engine.board.legal_moves:
-            engine.board.push(move_d7d5)
-            print("\n--- After Black plays d7d5 (No Capture) ---")
-            engine.display_board()
-            score_after_d7d5 = engine.evaluate_material()
-            # Should still be 0
-            print(f"Material Evaluation: {score_after_d7d5}")
+    #     move_d7d5 = chess.Move.from_uci("d7d5")
+    #     if move_d7d5 in engine.board.legal_moves:
+    #         engine.board.push(move_d7d5)
+    #         print("\n--- After Black plays d7d5 (No Capture) ---")
+    #         engine.display_board()
+    #         score_after_d7d5 = engine.evaluate_material()
+    #         # Should still be 0
+    #         print(f"Material Evaluation: {score_after_d7d5}")
 
-        # Capturing move
-        # White plays exd5 (pawn capture pawn)
-        move_exd5 = chess.Move.from_uci("e4d5")
-        if move_exd5 in engine.board.legal_moves:
-            engine.board.push(move_exd5)
-            print("\n--- After White plays exd5 (Pawn captures Pawn) ---")
-            engine.display_board()
-            score_after_capture = engine.evaluate_material()
-            # Should be 1 (White is winning)
-            print(f"Material Evaluation: {score_after_capture}")
+    #     # Capturing move
+    #     # White plays exd5 (pawn capture pawn)
+    #     move_exd5 = chess.Move.from_uci("e4d5")
+    #     if move_exd5 in engine.board.legal_moves:
+    #         engine.board.push(move_exd5)
+    #         print("\n--- After White plays exd5 (Pawn captures Pawn) ---")
+    #         engine.display_board()
+    #         score_after_capture = engine.evaluate_material()
+    #         # Should be 1 (White is winning)
+    #         print(f"Material Evaluation: {score_after_capture}")
 
-    except ValueError as e:
-        print(f"\nError processing move: {e}")
+    # except ValueError as e:
+    #     print(f"\nError processing move: {e}")
 
-    if engine.board.turn == chess.BLACK:
-        print("It is now Black's turn")
-    else:
-        print("It is not White's turn")
+    # if engine.board.turn == chess.BLACK:
+    #     print("It is now Black's turn")
+    # else:
+    #     print("It is not White's turn")
